b477cc2500520af2db910e9afe493834bb28a821
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2022-07-08 18:28:25 -0500

    backgrounds

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Logo from './components/logo/Logo'
+import Timer from './components/timer/Timer'
+import CTA from './components/cta/CTA'
+import Socials from './components/socials/Socials'
+import BG from './assets/bg01.jpg'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Timer />
+      <CTA />
+      <Socials />
+    </div>
+  );
+}
+
+export default App;
